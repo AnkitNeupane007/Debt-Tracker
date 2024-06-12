edf50b688cd9a1bd5a3f5edf7390562f6e2e68ce
AnkitNeupane007 <135531183+AnkitNeupane007@users.noreply.github.com>
2024-06-12 16:42:45 +0545

    csrf check

--- crm/settings.py
+++ crm/settings.py
@@ -130,11 +130,11 @@ EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-# HTTPS Configuration
-SECURE_SSL_REDIRECT = True
+# # HTTPS Configuration
+# SECURE_SSL_REDIRECT = True
 
-# Session Cookie Settings
-SESSION_COOKIE_SECURE = True
+# # Session Cookie Settings
+# SESSION_COOKIE_SECURE = True
 
 # Cookie SameSite Policy
 CSRF_COOKIE_SAMESITE = 'Strict'
